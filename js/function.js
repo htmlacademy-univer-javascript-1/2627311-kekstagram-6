6d80c0b815dfa295bbb285a09ba321dfc0077042
Семён Чазов <cemenchazov@gmail.com>
2025-09-28 18:59:19 +0500

    2.20

---
+++ js/function.js
@@ -0,0 +1,13 @@
+const compareLengthString = (string, length) =>  string.length <= length;
+
+const checkPalindrom = (string) => {
+  const normalString = string.replaceAll(' ', '').toLowerCase();
+  let reverseString = '';
+  for (let i = normalString.length - 1; i >= 0; i--){
+    reverseString += normalString[i];
+  }
+  return normalString === reverseString;
+};
+
+console.log(compareLengthString('олень', 5));
+console.log(checkPalindrom('топот', 'топот'));
